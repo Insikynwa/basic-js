a5ce7612271ffa2b2da621cc926d9fcfe6cd78ca
Insikynwa <pavel.churkin@mail.ru>
2021-03-28 01:11:18 +0300

    implement carbn-datig function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,21 @@
-const CustomError = require("../extensions/custom-error");
-
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+
+  let result;
+  const k = 0.693 /  HALF_LIFE_PERIOD;
+  if (
+    sampleActivity &&
+    MODERN_ACTIVITY > sampleActivity &&
+    typeof sampleActivity === "string" &&
+    sampleActivity > 0
+
+) 
+    {
+    result = Math.log(MODERN_ACTIVITY/sampleActivity) / k;
+    return result > 0 ? Math.ceil(result) : false;
+    }
+else return false
+
 };
