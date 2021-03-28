2b277581ba3f0371435dd36b07895d4f3174a0ec
Insikynwa <pavel.churkin@mail.ru>
2021-03-29 01:56:27 +0300

    feat: implement simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    resultArray: [],
+    getLength() {
+        const chainLength = chainMaker.resultArray.length;
+        return chainLength;
+    },
+    addLink(value) {
+        chainMaker.resultArray.push(value);
+        return chainMaker;
+    },
+    removeLink(position) {
+        const realPosition = position - 1;
+        if (
+            !Number.isInteger(realPosition) ||
+            typeof realPosition !== "number" ||
+            realPosition < 0 ||
+            realPosition > chainMaker.getLength()
+        ) {
+            chainMaker.resultArray = [];
+            throw new Error("FAIL");
+        } else {
+            chainMaker.resultArray.splice(realPosition, 1);
+            return chainMaker;
+        }
+    },
+    reverseChain() {
+        chainMaker.resultArray.reverse();
+        return chainMaker;
+    },
+    finishChain() {
+        let result = "";
+        chainMaker.resultArray.forEach((element) => {
+            result += "( " + element + " )~~";
+        });
+
+        chainMaker.resultArray = [];
+        return result.slice(0, -2);
+    },
 };
 
 module.exports = chainMaker;
