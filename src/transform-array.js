e06e83a1a60a3156cc44007f14a500913bafb972
Insikynwa <pavel.churkin@mail.ru>
2021-03-28 21:35:50 +0300

    feat: implement transform function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+    const result = [];
+    if (Array.isArray(arr) === false) {
+        throw new Error("FAIL");
+    } else {
+        for (let i = 0; i < arr.length; i++) {
+            switch (arr[i]) {
+                case "--discard-next":
+                    i + 1 < arr.length ? i++ && result.push(undefined) : i;
+                    break;
+                case "--discard-prev":
+                    i > 0 ? result.pop() : i;
+                    break;
+                case "--double-next":
+                    i + 1 < arr.length ? result.push(arr[i + 1]) : i;
+                    break;
+                case "--double-prev":
+                    i > 0 ? result.push(result[result.length - 1]) : i;
+                    break;
+                default:
+                    result.push(arr[i]);
+            }
+        }
+    }
+    return result.filter((item) => item !== undefined);
 };
