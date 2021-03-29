26a96f608c2917422e6be5d27402dc8c71ae23bb
Insikynwa <pavel.churkin@mail.ru>
2021-03-30 01:38:09 +0300

    implement vigenere-cipher function

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,50 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(isDirect = true) {
+        this.isDirect = isDirect;
+    }
+
+    encrypt(message, key) {
+        return this.encryptFunction(message, key, true);
+    }
+    decrypt(message, key) {
+        return this.encryptFunction(message, key, false);
+    }
+    encryptFunction(message, key, mode) {
+        let tempString = "";
+        if (message === undefined || key === undefined) {
+            throw new Error("FAIL");
+        }
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        var index = 0;
+        var step = 0;
+        var counter = 0;
+
+        while (index < message.length) {
+            if (/^[A-Z]$/.test(message[index])) {
+                counter = (index - step) % key.length;
+                var messageCode = message.charCodeAt(index);
+                var keyCode = key.charCodeAt(counter);
+                var modifElement = mode ? 1 : -1;
+                var newCode = (keyCode - 65) * modifElement + messageCode;
+                if (newCode < 65 || newCode > 90) {
+                    newCode = mode
+                        ? ((keyCode - 65 + messageCode) % 91) + 65
+                        : 90 - (64 - (newCode % 65));
+                }
+                tempString += String.fromCharCode(newCode);
+            } else {
+                tempString += message[index];
+                step++;
+            }
+            index++;
+        }
+        return this.isDirect
+            ? tempString
+            : String(tempString).split("").reverse().join("");
+    }
 }
 
 module.exports = VigenereCipheringMachine;
